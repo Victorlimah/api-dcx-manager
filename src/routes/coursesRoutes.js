e86fcc959495dcdf9f2fdd9c86950db2895e18fe
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-06-27 22:56:32 -0300

    Fix: add listCourses in route

--- src/routes/coursesRoutes.js
+++ src/routes/coursesRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listCenters } from "../controllers/coursesController.js";
+import { listCenters, listCourses } from "../controllers/coursesController.js";
 
 const coursesRouter = Router();
 
 coursesRouter.get("/centers", listCenters);
-coursesRouter.get("/courses/:centerId");
+coursesRouter.get("/courses/:centerId", listCourses);
 
 export default coursesRouter;
